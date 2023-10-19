0ccb09c540d6df6452a0f06c6703cee9893e8285
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2023-10-19 15:44:28 +0700

    fix

--- src/pages/user/DetailOrder.jsx
+++ src/pages/user/DetailOrder.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 
 import getImageUrl from "../../utils/imageGetter";
 import NavbarLogin from "../../components/NavbarLogin";
@@ -14,6 +16,38 @@ function DetailOrder() {
   const [Message, setMessage] = useState({ msg: null, isError: null });
   const [openModal, setOpenModal] = useState(false);
   const [isDropdownShown, setIsDropdownShow] = useState(false);
+
+  const [searchParams] = useSearchParams();
+  const url = import.meta.env.VITE_BACKEND_HOST;
+  const token = localStorage.getItem("token");
+  const id = localStorage.getItem("userInfo");
+  const paramsId = searchParams.get("id");
+  const authAxios = axios.create({
+    baseURL: url,
+    headers: {
+      Authorization: `Barer ${token}`,
+    },
+  });
+
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    authAxios
+      .get("/orders/users/" + paramsId)
+      .then((res) => {
+        setUser(res.data.result[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const [order, setOrder] = useState([]);
+  useEffect(() => {
+    authAxios
+      .get("/orders/detail/" + paramsId)
+      .then((res) => {
+        setOrder(res.data.result);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <NavbarLogin
@@ -26,10 +60,10 @@ function DetailOrder() {
       />
       <header className="font-plusJakartaSans py-8 px-5 md:px-24 lg:py-10 lg:px-[130px] min-[1400px]:pt-[78px] min-[1400px]:pb-[64px]">
         <h1 className="text-2xl font-medium text-dark md:text-3xl lg:text-3xl xl:text-5xl">
-          Order <span className="font-bold">#12354-09893</span>
+          Order <span className="font-bold">#{paramsId}</span>
         </h1>
         <p className="text-base font-normal text-secondary mt-2">
-          21 March 2023 at 10:30 AM
+          {user.created_at}
         </p>
       </header>
       <main className="font-plusJakartaSans px-5 flex flex-col gap-y-12 md:px-24 lg:px-[130px] lg:flex-row lg:gap-x-5 justify-between">
@@ -50,7 +84,7 @@ function DetailOrder() {
               </div>
               <p className="flex-1 text-secondary font-medium">Fullname</p>
               <p className="flex-1 text-dark font-bold text-right">
-                Ghaluh Wizard Anggoro
+                {user.users_fullname}
               </p>
             </div>
             <div className="flex gap-x-3 items-center border-b border-[#e8e8e8e8] py-5 px-3.5">
@@ -63,7 +97,7 @@ function DetailOrder() {
               </div>
               <p className="flex-1 text-secondary font-medium">Address</p>
               <p className="flex-1 text-dark font-bold text-right">
-                Griya bandung indah
+                {user.users_address}
               </p>
             </div>
             <div className="flex gap-x-3 items-center border-b border-[#e8e8e8e8] py-5 px-3.5">
@@ -76,7 +110,7 @@ function DetailOrder() {
               </div>
               <p className="flex-1 text-secondary font-medium">Phone</p>
               <p className="flex-1 text-dark font-bold text-right">
-                082116304338
+                {user.users_phone}
               </p>
             </div>
             <div className="flex gap-x-3 items-center border-b border-[#e8e8e8e8] py-5 px-3.5">
@@ -88,7 +122,7 @@ function DetailOrder() {
                 />
               </div>
               <p className="flex-1 text-secondary font-medium">
-                Payment Method
+                {user.payment_methods_name}
               </p>
               <p className="flex-1 text-dark font-bold text-right">Cash</p>
             </div>
@@ -101,7 +135,9 @@ function DetailOrder() {
                 />
               </div>
               <p className="flex-1 text-secondary font-medium">Shipping</p>
-              <p className="flex-1 text-dark font-bold text-right">Dine In</p>
+              <p className="flex-1 text-dark font-bold text-right">
+                {user.deliveries_name}
+              </p>
             </div>
             <div className="flex gap-x-3 items-center border-b border-[#e8e8e8e8] py-5 px-3.5">
               <div className="flex-none">
@@ -113,12 +149,14 @@ function DetailOrder() {
               </div>
               <p className="flex-1 text-secondary font-medium">Status</p>
               <p className="text-sm text-dark font-bold py-2 px-4 rounded-full bg-[#00A70033]">
-                Done
+                {user.orders_status}
               </p>
             </div>
             <div className="flex gap-x-3 items-center justify-between py-5 px-3.5">
               <p className="text-secondary font-medium">Total Transaksi</p>
-              <p className="text-sm text-dark font-bold">Idr 40.000</p>
+              <p className="text-sm text-dark font-bold">
+                IDR. {user.orders_total}
+              </p>
             </div>
           </section>
         </section>
@@ -129,62 +167,42 @@ function DetailOrder() {
             </h2>
           </header>
           <section className="flex flex-col gap-y-4">
-            <div className="item-product bg-[#E8E8E84D] flex flex-col gap-y-4 p-4 sm:flex-row sm:gap-x-4 sm:items-center">
-              <div className="w-[80%] h-[80%] sm:w-[60%] sm:h-[60%] lg:w-1/3">
-                <img
-                  src={getImageUrl("image31", "webp")}
-                  alt="coffee"
-                  className="w-full h-full"
-                />
-              </div>
-              <div className="product-info flex flex-col items-start gap-y-4 lg:w-2/3">
-                <p className="text-xs text-light bg-[#D00000] p-[10px] rounded-full font-bold">
-                  FLASH SALE!
-                </p>
-                <p className="text-lg text-[#0B0909] font-bold">
-                  Hazelnut Latte
-                </p>
-                <div className="text-sm text-secondary font-normal md:text-lg lg:text-base xl:text-lg flex">
-                  <p>2pcs | Regular | Ice | DineIn</p>
+            {order.map((result, i) => (
+              <div
+                className="bg-[#E8E8E84D] flex flex-col gap-y-4 p-4 sm:flex-row sm:gap-x-4 sm:items-center"
+                key={i}
+              >
+                <div className="w-[80%] h-[80%] sm:w-[60%] sm:h-[60%] lg:w-1/3">
+                  <img
+                    src={result.products_image}
+                    alt="coffee"
+                    className="w-full h-full"
+                  />
                 </div>
-                <div className="flex gap-x-4 items-center">
-                  <p className="text-xs text-[#D00000] font-medium line-through">
-                    IDR40.000
+                <div className="product-info flex flex-col items-start gap-y-4 lg:w-2/3">
+                  <p className="text-xs text-light bg-[#D00000] p-[10px] rounded-full font-bold">
+                    FLASH SALE!
                   </p>
-                  <p className="text-lg text-[#0B0909] font-medium lg:text-[22px]">
-                    IDR20.000
+                  <p className="text-lg text-[#0B0909] font-bold">
+                    {result.products_name}
                   </p>
-                </div>
-              </div>
-            </div>
-            <div className="item-product bg-[#E8E8E84D] flex flex-col gap-y-4 p-4 sm:flex-row sm:gap-x-4 sm:items-center">
-              <div className="w-[80%] h-[80%] sm:w-[60%] sm:h-[60%] lg:w-1/3">
-                <img
-                  src={getImageUrl("image31", "webp")}
-                  alt="coffee"
-                  className="w-full h-full"
-                />
-              </div>
-              <div className="product-info flex flex-col items-start gap-y-4 lg:w-2/3">
-                <p className="text-xs text-light bg-[#D00000] p-[10px] rounded-full font-bold">
-                  FLASH SALE!
-                </p>
-                <p className="text-lg text-[#0B0909] font-bold">
-                  Hazelnut Latte
-                </p>
-                <div className="text-sm text-secondary font-normal md:text-lg lg:text-base xl:text-lg flex">
-                  <p>2pcs | Regular | Ice | DineIn</p>
-                </div>
-                <div className="flex gap-x-4 items-center">
-                  <p className="text-xs text-[#D00000] font-medium line-through">
-                    IDR40.000
-                  </p>
-                  <p className="text-lg text-[#0B0909] font-medium lg:text-[22px]">
-                    IDR20.000
-                  </p>
+                  <div className="text-sm text-secondary font-normal md:text-lg lg:text-base xl:text-lg flex">
+                    <p>
+                      {result.orders_products_qty} Pcs | {result.sizes_name} |
+                      Ice | DineIn
+                    </p>
+                  </div>
+                  <div className="flex gap-x-4 items-center">
+                    <p className="text-xs text-[#D00000] font-medium line-through">
+                      IDR40.000
+                    </p>
+                    <p className="text-lg text-[#0B0909] font-medium lg:text-[22px]">
+                      IDR. {result.products_price}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </section>
         </section>
       </main>
