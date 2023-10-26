94b8f452f10ba5c82a1c8e1cd69c84eab1b27739
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2023-10-27 03:10:40 +0700

    add redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import router from "./Router.jsx";
-// import { UserProvider } from "./context/UserContext.jsx";
 import { ProductProvider } from "./context/ProductContext.jsx";
 import { Provider as ReduxProvider } from "react-redux";
-import reduxStore from "./redux/store";
+// import reduxStore from "./redux/store";
+import { store, persistedStore } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ReduxProvider store={reduxStore}>
-      <ProductProvider>
-        <RouterProvider router={router} />
-      </ProductProvider>
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <ProductProvider>
+          <RouterProvider router={router} />
+        </ProductProvider>
+      </PersistGate>
     </ReduxProvider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userReducer from "./slice/user";
 
-// const initialState = {
-//   data: {
-//     users_id: null,
-//     roles_id: null,
-//     token: null,
-//   },
-// };
+const persistConfig = {
+  key: "user",
+  storage,
+};
 
-// const reduxStore = configureStore({
-//   reducer: (prevState = initialState, action) => {
-//     // token: (add)
-//     // action: {
-//     //   type: (get)
-//     //   data: token
-//     switch (action.type) {
-//       case "ADD":
-//         return {
-//           ...prevState,
-//           data: prevState.data,
-//         };
-//       case "GET":
-//         return {
-//           ...prevState,
-//           data: prevState.data,
-//         };
-//       default:
-//         return prevState;
-//     }
-//   },
-// });
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
 });
 
-export default store;
+export const persistedStore = persistStore(store);
